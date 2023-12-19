0305ed9f2c1963fdc3637829788d5f5bfcf3999c
Farhad rasouli <rasouli.farhad@gmail.com>
2023-12-19 18:40:45 +0330

    updated

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -2,8 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
+import { environment } from 'src/environments/environment';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+// const baseUrl = 'http://localhost:8080/api/tutorials';
+const BASE_URL = `{environment.baseUrl}/api/tutorials`; ;
 
 @Injectable({
   providedIn: 'root',
@@ -12,30 +14,30 @@ export class TutorialService {
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<Tutorial[]> {
-    return this.http.get<Tutorial[]>(baseUrl);
+    return this.http.get<Tutorial[]>(BASE_URL);
   }
 
   get(id: any): Observable<Tutorial> {
-    return this.http.get<Tutorial>(`${baseUrl}/${id}`);
+    return this.http.get<Tutorial>(`${BASE_URL}/${id}`);
   }
 
   create(data: any): Observable<any> {
-    return this.http.post(baseUrl, data);
+    return this.http.post(BASE_URL, data);
   }
 
   update(id: any, data: any): Observable<any> {
-    return this.http.put(`${baseUrl}/${id}`, data);
+    return this.http.put(`${BASE_URL}/${id}`, data);
   }
 
   delete(id: any): Observable<any> {
-    return this.http.delete(`${baseUrl}/${id}`);
+    return this.http.delete(`${BASE_URL}/${id}`);
   }
 
   deleteAll(): Observable<any> {
-    return this.http.delete(baseUrl);
+    return this.http.delete(BASE_URL);
   }
 
   findByTitle(title: any): Observable<Tutorial[]> {
-    return this.http.get<Tutorial[]>(`${baseUrl}?title=${title}`);
+    return this.http.get<Tutorial[]>(`${BASE_URL}?title=${title}`);
   }
 }
